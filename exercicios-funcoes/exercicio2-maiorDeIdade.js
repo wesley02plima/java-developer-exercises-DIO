d7dbfe90fc07bc9ef5e2b3f3d8323ec7563cd80d
wesley02plima <wesley02.pereira@gmail.com>
2026-06-14 21:13:21 -0300

    exercicio funcoes

---
+++ exercicios-funcoes/exercicio2-maiorDeIdade.js
@@ -0,0 +1,10 @@
+function verificacaoIdade(idade) {
+    if (idade < 18) {
+        console.log("Menor de idade");
+    }
+    else {
+        console.log("Maior de idade");
+    }
+}
+
+verificacaoIdade(30);
