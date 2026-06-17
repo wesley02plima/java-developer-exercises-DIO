ad034a0cb6a846b63cc8c399d60eb824ea8c35af
wesley02plima <wesley02.pereira@gmail.com>
2026-06-17 20:57:24 -0300

    Exercicios de classe e objeto

---
+++ exercicios-objeto/exercicio-classe-carro.js
@@ -0,0 +1,30 @@
+const prompt = require('prompt-sync')();
+
+class Carro {
+    constructor(marca,cor,distanciaPorLitro) {
+        this.marca = marca;
+        this.cor = cor;
+        this.distanciaPorLitro = distanciaPorLitro;
+    }
+    custoViagem() {
+        let precoCombustivel = parseFloat(prompt("Preço do combustível por litro(R$): "));
+        let distanciaViagem = parseFloat(prompt("Distância da viagem(KM): "));
+        let custoViagem = (distanciaViagem/this.distanciaPorLitro) * precoCombustivel
+
+        console.log("Custo da viagem: R$ " + custoViagem.toFixed(2));
+    }
+    descricaoCarro() {
+        console.log("O carro da marca " + this.marca + " cor " + this.cor + " faz " + this.distanciaPorLitro + "KM por litro.")
+    }
+}
+
+const meuCarro = new Carro("BMW","azul",16);
+meuCarro.custoViagem(); 
+meuCarro.descricaoCarro();
+
+
+
+
+
+
+
