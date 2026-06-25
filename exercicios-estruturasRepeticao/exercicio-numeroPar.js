1df79e064a1fef94a743cf530cecbf1562c9a40a
wesley02plima <wesley02.pereira@gmail.com>
2026-06-25 19:08:40 -0300

    Exercicios de estruturas de repetição

---
+++ exercicios-estruturasRepeticao/exercicio-numeroPar.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+
+let array = parseInt(prompt("Quantidade de números: "));
+let numeros = new Array (array);
+
+for (let i = 0; i <= numeros.length; i++) {
+    if (i % 2 === 0) {
+
+        console.log(i);
+    }
+    
+}
+
+
+// correção: 
+//const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+// for (let i = 0; i < numeros.length; i++) {
+//  const = numero = numeros[i];
+//  if (numero % 2 === 0) {
+//      console.log("numero[i]");
+//  }
+//}
